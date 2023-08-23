2fafdd7e6282e683e3db09f550ad63e9d55b405f
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-08-23 23:58:22 +0530

    Date object in js

---
+++ 02-Js-Fundamnet-02/08-Date.js
@@ -0,0 +1,21 @@
+// let dateTime = new Date()
+// console.log(dateTime);
+
+// console.log(Date.now());
+
+// console.log(dateTime.getDate());
+// console.log(dateTime.get);
+
+// const birtDate = '1999-01-01'
+// const date = new Date(birtDate)
+
+// console.log(date.getDay());
+
+let newDate =new Date();
+console.log(newDate.toDateString());
+console.log(newDate.toLocaleString());
+console.log(newDate.toUTCString());
+
+
+
+
