4258da31aabf8c9eb80a108e47766ffd7ebcdd2e
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-08-23 23:57:44 +0530

    build in math Object method

---
+++ 02-Js-Fundamnet-02/07-BuildInMathObj.js
@@ -0,0 +1,57 @@
+// const negative = -20;
+// console.log(Math.abs(negative));
+
+// console.log(Math.trunc(4.5))
+
+// console.log(Math.sqrt(6));
+// console.log(Math.round(3.5));
+// console.log(Math.ceil(4.9));
+// console.log(Math.floor(5.9));
+
+
+// console.log(Math.PI);
+
+// console.log(Math.pow(2,4));
+// console.log(Math.min(3,4,6,2));
+// console.log(Math.max(4,5,678,32,23));
+
+//***************  String 
+
+// console.log("${10+30}");
+// console.log('${10 + 40}');
+// console.log(`${10 + 40}`);
+
+
+// console.log('hello world'.length);
+// console.log('hello world'[6]);
+
+//*****************   */ Strings are immutable
+
+const str = "Rupesh";
+str[0] = "j";
+// console.log(str);
+console.log(str.charAt(2));
+
+// console.log(str.endsWith("Esh"));
+
+// console.log(str.padEnd(15,"!"));
+// console.log(str.padStart(15,"!"));
+
+// console.log(str.repeat(3));
+
+// console.log("hello world world".replace('world',"everyone"));
+// console.log("hello world world".replaceAll('world',"everyone"));
+
+// console.log("Tata consoltancy".slice(5));
+// console.log("Tata consoltancy".slice(5,9));
+
+
+// console.log("Tata consoltancy".substring(5,9));
+
+// console.log("Tcs,wipro,accentuure,infosys".split(","));
+
+// console.log("         helllo  ".trim());
+
+
+
+
