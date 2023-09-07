ad3dea444ee7a3781b852e9e5f4fa56885b3f4ff
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-09-07 14:33:20 +0000

    async await and fetch

---
+++ Week-17-PromisesAndsCallBack/03-asynAwait.js
@@ -0,0 +1,38 @@
+// function creatPromise(){
+//         return new Promise(function exec(resolve,reject){
+//               setTimeout(function f(){
+//                 console.log("timer DOne")
+//                 // resolve(10)
+//                 reject(60)
+//               },3000)
+//         })
+//     }
+
+// async function consume(){
+     
+//     // console.log("inside function");
+//     // const response = await creatPromise();
+//     // console.log("response is", response);
+
+//     try{
+//        console.log("async")
+//        const response = await creatPromise();
+//        const response1 = await  creatPromise();
+//        console.log("repsonse is ", response);
+//     } catch(err){
+//         console.log("handel",err)
+//     }
+// }
+
+
+
+
+
+// console.log("start")
+
+// console.log(consume())
+
+// console.log("end");
+
+
+
